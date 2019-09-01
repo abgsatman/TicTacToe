abf06548b926036541476161a4885e5f2e0cb9ac
abgsatman <abgsatman@hotmail.com>
2019-09-01 22:44:33 +0300

    enum is edited.

--- Scripts/Data/EnumData.cs
+++ Scripts/Data/EnumData.cs
@@ -4,34 +4,6 @@
 * This file is a part of TicTacToe project.
 */
 
-public enum Positions
-{
-    s1,
-    s2,
-    s3,
-    s4,
-    s5,
-    s6,
-    s7,
-    s8,
-    s9,
-    none
-}
-
-public enum Symbols
-{
-    none,
-    X,
-    O
-}
-
-public enum PlayerID
-{
-    none,
-    PlayerA,
-    PlayerB
-}
-
 public enum GameState
 {
     Login,
